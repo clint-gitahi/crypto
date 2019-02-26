9b9cbc3b3672376d91e983ce7088af646f3b4673
clintgitahi <clinton@yumkenya.com>
2019-02-26 05:32:14 +0300

    coin model and singleton

---
+++ CryptoTracker/CoinData.swift
@@ -0,0 +1,29 @@
+
+import UIKit
+
+// singleton
+class CoinData {
+    static let shared = CoinData()
+    var coins = [Coin]()
+    
+    private init() {
+        let symbols = ["BTC", "ETH", "LTC"]
+        
+        for symbol in symbols {
+            let coin = Coin(symbol: symbol)
+            coins.append(coin)
+        }
+    }
+}
+
+class Coin {
+    var symbol = ""
+    var image = UIImage()
+    var price = 0.0
+    var amount = 0.0
+    var historicalData = [Double]()
+    
+    init(symbol: String) {
+        self.symbol = symbol
+    }
+}
