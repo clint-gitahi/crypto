9ff1cd6cd040365bd87c1ba86ce1871029304b8e
clintgitahi <clinton@yumkenya.com>
2019-03-04 07:20:17 +0300

    adding alert

--- CryptoTracker/CoinViewController.swift
+++ CryptoTracker/CoinViewController.swift
@@ -29,6 +29,10 @@ class CoinViewController: UIViewController, CoinDataDelegate {
             
             view.backgroundColor = UIColor.white
             edgesForExtendedLayout = []
+            title = coin.symbol
+            
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editTapped))
+            
             chart.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: chartHeight)
             chart.yLabelsFormatter = { CoinData.shared.doubleToMoneyString(double: $1)}
             chart.xLabels = [0, 5, 10, 15, 20, 25, 30]
@@ -42,23 +46,48 @@ class CoinViewController: UIViewController, CoinDataDelegate {
             view.addSubview(imageView)
             
             priceLabel.frame = CGRect(x: 0, y: chartHeight+imageSize, width: view.frame.size.width, height: priceLabelHeight)
-            priceLabel.text = coin.priceAsString()
             priceLabel.textAlignment = .center
             view.addSubview(priceLabel)
             
             yourOwnLabel.frame = CGRect(x: 0, y: chartHeight + imageSize + priceLabelHeight * 2, width: view.frame.size.width, height: priceLabelHeight)
             yourOwnLabel.textAlignment = .center
             yourOwnLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
-            yourOwnLabel.text = "You Own: \(coin.amount) \(coin.symbol)"
+            
             view.addSubview(yourOwnLabel)
             
             worthLabel.frame = CGRect(x: 0, y: chartHeight + imageSize + priceLabelHeight * 3, width: view.frame.size.width, height: priceLabelHeight)
             worthLabel.textAlignment = .center
             worthLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
-            worthLabel.text = coin.amountAsString()
+            
             view.addSubview(worthLabel)
             
             coin.getHistoricalData()
+            newPrices()
+        }
+    }
+    
+    @objc func editTapped() {
+        if let coin = coin {
+            let alert = UIAlertController(title: "How Much \(coin.symbol) do you own", message: nil, preferredStyle: .alert)
+            
+            alert.addTextField { (textField) in
+                textField.placeholder = "0.5"
+                textField.keyboardType = .decimalPad
+                if self.coin?.amount != 0.0 {
+                    textField.text = String(coin.amount)
+                }
+            }
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {(action) in
+                
+                if let text = alert.textFields?[0].text {
+                    if let amount = Double(text) {
+                        self.coin?.amount = amount
+                        self.newPrices()
+                    }
+                }
+            }))
+            self.present(alert, animated: true, completion: nil)
         }
     }
     
@@ -71,7 +100,11 @@ class CoinViewController: UIViewController, CoinDataDelegate {
     }
     
     func newPrices() {
-        priceLabel.text = coin?.priceAsString()
+        if let coin = coin {
+            priceLabel.text = coin.priceAsString()
+            worthLabel.text = coin.amountAsString()
+            yourOwnLabel.text = "You Own: \(coin.amount) \(coin.symbol)"
+        }
     }
     
 }
